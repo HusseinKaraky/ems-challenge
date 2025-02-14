e296201d67d8a1f795404b24bff0ce9161c232ea
HusseinKaraky <hussein_karaky@outlook.com>
2025-02-14 21:52:39 +0200

    seed.js

--- scripts/seed.js
+++ scripts/seed.js
@@ -10,21 +10,43 @@ const __dirname = path.dirname(__filename);
 const dbConfigPath = path.join(__dirname, '../database.yaml');
 const dbConfig = yaml.load(fs.readFileSync(dbConfigPath, 'utf8'));
 
-const {
-  'sqlite_path': sqlitePath,
-} = dbConfig;
+const { sqlite_path: sqlitePath } = dbConfig;
 
 const db = new sqlite3.Database(sqlitePath);
 
 const employees = [
   {
-    full_name: 'John Doe'
+    full_name: 'John Doe',
+    email: 'john.doe@example.com',
+    phone_number: '123-456-7890',
+    date_of_birth: '1990-01-01',
+    job_title: 'Software Engineer',
+    department: 'Engineering',
+    salary: 80000,
+    start_date: '2020-01-01',
+    end_date: null, 
   },
   {
-    full_name: 'Jane Smith'
+    full_name: 'Jane Smith',
+    email: 'jane.smith@example.com',
+    phone_number: '987-654-3210',
+    date_of_birth: '1985-05-15',
+    job_title: 'Product Manager',
+    department: 'Product',
+    salary: 90000,
+    start_date: '2019-06-01',
+    end_date: null, 
   },
   {
-    full_name: 'Alice Johnson'
+    full_name: 'Alice Johnson',
+    email: 'alice.johnson@example.com',
+    phone_number: '555-555-5555',
+    date_of_birth: '1995-12-25',
+    job_title: 'UX Designer',
+    department: 'Design',
+    salary: 75000,
+    start_date: '2021-03-15',
+    end_date: null, 
   },
 ];
 
@@ -46,14 +68,13 @@ const timesheets = [
   },
 ];
 
-
 const insertData = (table, data) => {
   const columns = Object.keys(data[0]).join(', ');
   const placeholders = Object.keys(data[0]).map(() => '?').join(', ');
 
   const insertStmt = db.prepare(`INSERT INTO ${table} (${columns}) VALUES (${placeholders})`);
 
-  data.forEach(row => {
+  data.forEach((row) => {
     insertStmt.run(Object.values(row));
   });
 
@@ -61,15 +82,42 @@ const insertData = (table, data) => {
 };
 
 db.serialize(() => {
+  db.run('DROP TABLE IF EXISTS employees');
+  db.run('DROP TABLE IF EXISTS timesheets');
+
+  db.run(`
+    CREATE TABLE employees (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      full_name TEXT NOT NULL,
+      email TEXT NOT NULL,
+      phone_number TEXT,
+      date_of_birth DATE,
+      job_title TEXT NOT NULL,
+      department TEXT NOT NULL,
+      salary INTEGER,
+      start_date DATE NOT NULL,
+      end_date DATE
+    )
+  `);
+
+  db.run(`
+    CREATE TABLE timesheets (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      start_time DATETIME NOT NULL,
+      end_time DATETIME NOT NULL,
+      employee_id INTEGER NOT NULL,
+      FOREIGN KEY (employee_id) REFERENCES employees(id)
+    )
+  `);
+
   insertData('employees', employees);
   insertData('timesheets', timesheets);
 });
 
-db.close(err => {
+db.close((err) => {
   if (err) {
     console.error(err.message);
   } else {
     console.log('Database seeded successfully.');
   }
 });
-
